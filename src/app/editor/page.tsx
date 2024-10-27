f376a8d3446d4ba45e4338bb25a4e2a21187190e
jr <fernando.tabamo@whitecobalt.com>
2024-10-27 23:29:26 +0800

    CV Builder No SSR

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,14 +1,14 @@
 import dynamic from 'next/dynamic'
-import { CVBuilder as OGBuilder } from "./(components)/cv-builder";
+import { CVBuilder } from "./(components)/cv-builder";
 
-const CVBuilder = dynamic(() => Promise.resolve(OGBuilder), {
+const CVBuilderNoSSR = dynamic(() => Promise.resolve(CVBuilder), {
   ssr: false
 })
 
 export default function Editor() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-emerald-100 to-emerald-300">
-      <CVBuilder />
+      <CVBuilderNoSSR />
     </div>
   );
 }
