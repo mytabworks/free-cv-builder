9e22673dec6fde4002fed47db5d4bc7db611aa49
jr <fernando.tabamo@whitecobalt.com>
2024-11-02 21:33:13 +0800

    abstracted options

---
+++ src/constants/fonts.ts
@@ -0,0 +1,18 @@
+import { googleFonts } from "@/lib/page-renderer";
+
+export const fonts = [
+  ...googleFonts,
+  "Arial",
+  "Helvetica",
+  "Times New Roman",
+  "Courier",
+  "Verdana",
+  "Georgia",
+  "Palatino",
+  "Garamond",
+  "Bookman",
+  "Comic Sans MS",
+  "Trebuchet MS",
+  "Arial Black",
+  "Impact",
+]
---
+++ src/constants/templates.ts
@@ -0,0 +1,62 @@
+export const templates = [
+  {
+    label: "Modern V2",
+    value: "customize",
+  },
+  {
+    label: "Cyberpunk",
+    value: "cyberpunk",
+  },
+  {
+    label: "Classic",
+    value: "classic",
+  },
+  {
+    label: "Creative",
+    value: "creative",
+  },
+  {
+    label: "Flat design",
+    value: "flatdesign",
+  },
+  {
+    label: "Neumorphic",
+    value: "neumorphism",
+  },
+  {
+    label: "Modern",
+    value: "modern",
+  },
+  {
+    label: "Minimalist",
+    value: "minimalist",
+  },
+  {
+    label: "Professional",
+    value: "professional",
+  },
+]
+
+export const templateWithImages = [
+  {
+    id: "flatdesign",
+    title: "Flat Design",
+    imageUrl: "/images/flatdesign.jpg",
+  },
+  { id: "modern", title: "Modern", imageUrl: "/images/modern.jpg" },
+  { id: "creative", title: "Creative", imageUrl: "/images/creative.jpg" },
+  {
+    id: "professional",
+    title: "Professional",
+    imageUrl: "/images/professional.jpg",
+  },
+  { id: "minimalist", title: "Minimalist", imageUrl: "/images/minimalist.jpg" },
+  { id: "cyberpunk", title: "Cyberpunk", imageUrl: "/images/cyberpunk.jpg" },
+  {
+    id: "neumorphism",
+    title: "Neumorphic",
+    imageUrl: "/images/neumorphism.jpg",
+  },
+  { id: "classic", title: "Classic", imageUrl: "/images/classic.jpg" },
+  { id: "customize", title: "Modern V2", imageUrl: "/images/customize.jpg" },
+]
