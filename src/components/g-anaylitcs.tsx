c91fe7a79f71290581c802062007a7f1eac2b087
jr <fernando.tabamo@whitecobalt.com>
2024-11-04 19:35:25 +0800

    fix google analytics

--- src/components/g-anaylitcs.tsx
+++ src/components/g-anaylitcs.tsx
@@ -11,7 +11,7 @@ export function GAnalytics({ measurementId }: { measurementId: string }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${measurementId}', {
+            gtag('config', '${measurementId}');
           `,
         }}
       ></script>
