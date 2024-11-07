beb056066d4f900f83dd4e504660fd8d4117d261
jr <fernando.tabamo@whitecobalt.com>
2024-11-08 00:20:50 +0800

    fix gtag

--- src/lib/g-tag.ts
+++ src/lib/g-tag.ts
@@ -1,1 +1,2 @@
-export const gtag = (window as unknown as { gtag?: (type: string, name: string, params: Record<string, string>) => void; }).gtag
+
+export const gtag = typeof window !== 'undefined' ? (window as unknown as { gtag?: (type: string, name: string, params: Record<string, string>) => void; }).gtag : undefined
