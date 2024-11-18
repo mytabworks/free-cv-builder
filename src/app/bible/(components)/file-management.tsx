f7deb4c70a0a610b9bc80e61bb4df47039f14ed0
jr <fernando.tabamo@whitecobalt.com>
2024-11-18 19:21:45 +0800

    bible page

---
+++ src/app/bible/(components)/file-management.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Download } from "lucide-react";
+import React, { useState } from "react";
+
+export function FileManagement() {
+  const [files] = useState([
+    { version: '1.0.0', name: "Biblia-Pagbabawi.apk", url: "/apk/v1.apk" },
+  ]);
+
+  return (
+    <div className="p-6 min-h-screen container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Bible Recovery Tagalog Version</h1>
+      <div className="bg-white rounded-lg shadow p-4">
+        {files.map((file) => (
+          <div
+            key={file.version}
+            className="flex items-center justify-between border-b py-3"
+          >
+            <span className="text-gray-800 font-bold">v{file.version} - {file.name}</span>
+            <a
+              href={file.url}
+              download={file.name}
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 flex gap-2"
+            >
+              <Download /> Download
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/app/bible/page.tsx
@@ -0,0 +1,72 @@
+import { Link } from "@/components/link";
+import { PencilRulerIcon } from "lucide-react";
+import { Metadata } from "next";
+import Image from "next/image";
+import { FileManagement } from "./(components)/file-management";
+
+export const metadata: Metadata = {
+  title: "Free CV Builder | Create CV Online for Free",
+  description: "Build your professional CV online for free. No sign-up required. Fast, easy, and secure CV builder with customizable templates.",
+  keywords: "Free CV Builder, online CV builder, no sign-up CV builder, free resume builder, create CV for free, free CV maker, free resume maker",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://free-cv-builder.vercel.app/",
+    title: "Free CV Builder | Create CV Online for Free",
+    description: "Build your professional CV online for free. No sign-up required. Fast, easy, and secure CV builder with customizable templates.",
+    images: [
+      {
+        url: "https://free-cv-builder.vercel.app/free-cv-builder-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Free CV Builder | Create CV Online for Free",
+      },
+    ],
+    siteName: "Free CV Builder | Create CV Online for Free",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Free CV Builder | Create CV Online for Free",
+    description: "Build your professional CV online for free. No sign-up required. Fast, easy, and secure CV builder with customizable templates.",
+    images: [
+      {
+        url: "https://free-cv-builder.vercel.app/free-cv-builder-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Free CV Builder | Create CV Online for Free",
+      },
+    ],
+    site: "https://free-cv-builder.vercel.app/",
+    creator: "@mytabworks",
+  },
+};
+
+export default function LandingPage() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-emerald-100 to-teal-400">
+      <header className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center py-5 bg-white shadow-md">
+        <div className="container mx-auto flex justify-between items-center px-4">
+          <Link to="/">
+            <Image src="/free-cv-builder-logo.png" height={40} width={196} alt="Free CV Builder Logo" className="h-8 md:h-10 w-auto" />
+          </Link>
+          <nav className="hidden md:flex items-center">
+            <Link
+              to="/editor"
+              className="bg-neutral-900 text-white py-3 px-6 md:py-3 md:px-6 rounded-full text-sm md:text-lg hover:bg-neutral-800 transition-all duration-200 inline-flex items-center gap-2"
+            >
+              <b>Create Your CV Now</b><PencilRulerIcon className="inline size-4 sm:size-6"/>
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <div className="h-[72px]" />
+      <main className="mt-8 md:mt-12">
+        {/* Hero Section */}
+        <section className="text-center px-4 md:px-8">
+          <FileManagement />
+        </section>
+      </main>
+    </div>
+  );
+};
